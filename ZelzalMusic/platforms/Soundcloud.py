c82e7a3f25ef317689e748c403bc46b796029378
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2023-12-26 04:05:54 +0200

    Add files via upload

---
+++ ZelzalMusic/platforms/Soundcloud.py
@@ -0,0 +1,43 @@
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ 「𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾」 ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/T_Y_E_X   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/UI_VM ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+from os import path
+
+from yt_dlp import YoutubeDL
+
+from ZelzalMusic.utils.formatters import seconds_to_min
+
+
+class SoundAPI:
+    def __init__(self):
+        self.opts = {
+            "outtmpl": "downloads/%(id)s.%(ext)s",
+            "format": "best",
+            "retries": 3,
+            "nooverwrites": False,
+            "continuedl": True,
+        }
+
+    async def valid(self, link: str):
+        if "soundcloud" in link:
+            return True
+        else:
+            return False
+
+    async def download(self, url):
+        d = YoutubeDL(self.opts)
+        try:
+            info = d.extract_info(url)
+        except:
+            return False
+        xyz = path.join("downloads", f"{info['id']}.{info['ext']}")
+        duration_min = seconds_to_min(info["duration"])
+        track_details = {
+            "title": info["title"],
+            "duration_sec": info["duration"],
+            "duration_min": duration_min,
+            "uploader": info["uploader"],
+            "filepath": xyz,
+        }
+        return track_details, xyz
