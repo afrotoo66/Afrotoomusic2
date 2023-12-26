ecc127dcde4bd5f6e86c0983ef0d37703567977e
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2023-12-26 06:11:40 +0200

    Update play.py

--- ZelzalMusic/utils/inline/play.py
+++ ZelzalMusic/utils/inline/play.py
@@ -21,7 +21,7 @@ def track_markup(_, videoid, user_id, channel, fplay):
                 callback_data=f"MusicStream {videoid}|{user_id}|v|{channel}|{fplay}",
             ),
         ],
-        [InlineKeyboardButton(text=_["ZTHON_BUTTON"], url=f"https://t.me/ZThon_Music")],
+        [InlineKeyboardButton(text=_["ZTHON_BUTTON"], url=f"https://t.me/UI_VM")],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
@@ -71,7 +71,7 @@ def stream_markup_timer(_, chat_id, played, dur):
                 callback_data="GetTimer",
             )
         ],
-        [InlineKeyboardButton(text=_["ZTHON_BUTTON"], url=f"https://t.me/ZThon_Music")],
+        [InlineKeyboardButton(text=_["ZTHON_BUTTON"], url=f"https://t.me/UI_VM")],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
