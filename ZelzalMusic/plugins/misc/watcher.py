c4dadb3a2bf49c271d6267aaaa5ee347993a9655
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2023-12-26 03:40:19 +0200

    Add files via upload

---
+++ ZelzalMusic/plugins/misc/watcher.py
@@ -0,0 +1,18 @@
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ 「𝚂𝙾𝚞𝚁𝚂 𝙰𝙵𝚁𝙾𝚃𝙾𝙾」 ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯  T.me/T_Y_E_X   ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+#▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒✯ T.me/UI_VM ✯▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+from pyrogram import filters
+from pyrogram.types import Message
+
+from ZelzalMusic import app
+from ZelzalMusic.core.call import Zelzaly
+
+welcome = 20
+close = 30
+
+
+@app.on_message(filters.video_chat_started, group=welcome)
+@app.on_message(filters.video_chat_ended, group=close)
+async def welcome(_, message: Message):
+    await Zelzaly.stop_stream_force(message.chat.id)
