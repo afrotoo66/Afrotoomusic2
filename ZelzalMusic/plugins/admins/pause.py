8005eaa0e06ae3e122d08089a4e68daabaacd06f
afrotoo66 <131810077+afrotoo66@users.noreply.github.com>
2024-01-01 04:15:18 +0200

    Update pause.py

--- ZelzalMusic/plugins/admins/pause.py
+++ ZelzalMusic/plugins/admins/pause.py
@@ -14,13 +14,14 @@ from ZelzalMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
-@app.on_message(command(["وقف", "قف"]) & filters.group & ~BANNED_USERS)
+@app.on_message(command(["وقف", "قف"]) & ~BANNED_USERS)
 @AdminRightsCheck
 async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
+    user_mention = message.from_user.mention if message.from_user else "المشـرف"
     await Zelzaly.pause_stream(chat_id)
     await message.reply_text(
-        _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
+        _["admin_2"].format(user_mention), reply_markup=close_markup(_)
     )
